c17aedc8df521e8b2e5fb0f71e011825ee4668da
AGuyNamedJens <jens.wesseling@gmail.com>
2021-03-01 14:43:20 +0100

    remove old code

--- scripts/Index.js
+++ scripts/Index.js
@@ -15,64 +15,6 @@ function slideshow() {
   else index = 0;
 }
 
-/*var IntervalDonate;
-
-function slideInDonate() {
-  // aparte functie want een interval in een timeout wil niet werken
-  IntervalDonate = setInterval(fadeInDonate, 200);
-}
-
-function fadeInDonate() {
-  var body = document.getElementsByClassName("doneer");
-  var opacity = Number(
-    document.getElementsByClassName("doneer")[0].style.opacity
-  );
-
-  if (opacity < 1) {
-    opacity += 0.1;
-    body[0].style.opacity = opacity;
-  } else {
-    clearInterval(IntervalDonate);
-  }
-}
-
-var IntervalSlideShowOut;
-var IntervalSlideShowIn;
-
-function SlideShow() {
-  // aparte functie want een interval in een timeout wil niet werken
-  IntervalSlideShowOut = setInterval(fadeOutSlideShow, 200);
-}
-
-function fadeOutSlideShow() {
-  var body = document.getElementById("slideShowImg");
-  var opacity = Number(document.getElementById("slideShowImg").style.opacity);
-
-  if (opacity > 0.2) {
-    opacity -= 0.1;
-    body.style.opacity = opacity;
-  } else {
-    IntervalSlideShowIn = setInterval(fadeInSlideShow, 200);
-    slideshow();
-    clearInterval(IntervalSlideShowOut);
-  }
-}
-
-function fadeInSlideShow() {
-  var body = document.getElementById("slideShowImg");
-  var opacity = Number(document.getElementById("slideShowImg").style.opacity);
-
-  if (opacity < 1) {
-    opacity += 0.1;
-    body.style.opacity = opacity;
-  } else {
-    clearInterval(IntervalSlideShowIn);
-    setTimeout(() => {
-        IntervalSlideShowOut = setInterval(fadeOutSlideShow, 200);
-    }, 5000)
-  }
-}*/
-
 function donateButton() {
   document.getElementsByClassName("doneer")[0].className = "doneer fadeIn";
 }
