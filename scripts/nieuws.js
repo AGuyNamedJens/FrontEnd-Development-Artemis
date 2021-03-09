3eb7644d1fb0200b03a0c7115eb151be008a1122
Thomas Ruijters <Thomas.ruijters@edu-kw1c.nl>
2021-03-09 20:09:49 +0100

    nieuws en doneer 2 pagina
    Co-authored-by: Vincent Meegdes <w1-MeegdesVincent@users.noreply.github.com>

--- scripts/nieuws.js
+++ scripts/nieuws.js
@@ -11,8 +11,41 @@ $('#article1').click(function(){
     showArticle();
 })
 
+$('#article2').click(function(){
+    pText = $('#article2 p').text();
+    h2Text = $('#article2 h2').text();
+    imgSrc = $('#article2 img').attr('src');
+    // alert(imgSrc);
+    showArticle();
+})
+
+$('#article3').click(function(){
+    pText = $('#article3 p').text();
+    h2Text = $('#article3 h2').text();
+    imgSrc = $('#article3 img').attr('src');
+    // alert(imgSrc);
+    showArticle();
+})
+
+$('#article4').click(function(){
+    pText = $('#article4 p').text();
+    h2Text = $('#article4 h2').text();
+    imgSrc = $('#article4 img').attr('src');
+    // alert(imgSrc);
+    showArticle();
+})
+
+$('#article5').click(function(){
+    pText = $('#article5 p').text();
+    h2Text = $('#article5 h2').text();
+    imgSrc = $('#article5 img').attr('src');
+    // alert(imgSrc);
+    showArticle();
+})
+
 function showArticle(){
-    $('#showArticle img').attr("src", imgSrc);
-    $('#showArticle h2').text(h2Text);
-    $('#showArticle p').text(pText);
+    $('article').css("visibility","visible");
+    $('article img').attr("src", imgSrc);
+    $('article h2').text(h2Text);
+    $('article p').text(pText);
 }
