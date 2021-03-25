993eb95a56b05fe3ba66109eb98e9037758e4b44
AGuyNamedJens <jens.wesseling@gmail.com>
2021-03-25 21:54:02 +0100

    Fix merge (manually)

--- scripts/Index.js
+++ scripts/Index.js
@@ -2,11 +2,11 @@ let index = 0;
 
 function slideshow() {
   const dataArray = new Array(
-    "img/index/Slideshow/Pup.jpg",
-    "img/index/Slideshow/pup2.jpg",
-    "img/index/Slideshow/pup3.jpg",
-    "img/index/Slideshow/Cats_Kittens_Glance_549762_2560x1440.jpg",
-    "img/index/Slideshow/Pussy.jpg"
+    "Img/index/Slideshow/Pup.jpg",
+    "Img/index/Slideshow/pup2.jpg",
+    "Img/index/Slideshow/pup3.jpg",
+    "Img/index/Slideshow/Cats_Kittens_Glance_549762_2560x1440.jpg",
+    "Img/index/Slideshow/Pussy.jpg"
   );
   document.getElementById("slideShowImg").src = `${dataArray[`${index}`]}`;
   document.getElementById("slideShowImg").className = "fadeIn";
@@ -47,38 +47,4 @@ function SlideShow() {
 }
 
 // Timeouts voor fade effecten
-window.onload = setTimeout(BTNDonateAnimation, 1500);
 window.onload = setTimeout(SlideShow, 5000);
-
-window.onresize = (() => { BTNDonateAnimation() });
-
-let isLoaded = false;
-
-function BTNDonateAnimation()
-{ 
-  if(window.innerWidth >= 1100)
-  {
-    if(!isLoaded) document.getElementsByClassName("desktop")[0].classList.add("fadeIn");
-    else {
-      document.getElementsByClassName("desktop")[0].classList.remove("fadeIn");
-      document.getElementsByClassName("desktop")[0].style.opacity = '100%';
-    }
-
-    document.getElementsByClassName("desktop")[0].style.display = "flex";
-    document.getElementsByClassName("mobileDevice")[0].style.display = "none";
-  }
-
-  else if(window.innerWidth <= 1100)
-  {
-    if(!isLoaded) document.getElementsByClassName("mobileDevice")[0].classList.add("fadeIn");
-    else {
-      document.getElementsByClassName("mobileDevice")[0].classList.remove("fadeIn");
-      document.getElementsByClassName("mobileDevice")[0].style.opacity = '100%';
-    }
-
-    document.getElementsByClassName("mobileDevice")[0].style.display = "flex";
-    document.getElementsByClassName("desktop")[0].style.display = "none";
-  }
-
-  isLoaded = true;
-}
